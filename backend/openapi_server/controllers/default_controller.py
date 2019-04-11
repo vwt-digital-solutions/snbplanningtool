bf53f08879b56ed45e0578ddfaeb4acf0ee1aa81
Bernie van Veen <b.vanveen@vwt.digital>
2019-04-11 15:33:00 +0200

    Added models to swagger spec. CAR-25

--- backend/openapi_server/controllers/default_controller.py
+++ backend/openapi_server/controllers/default_controller.py
@@ -1,7 +1,3 @@
-import connexion
-import six
-
-from openapi_server import util
 
 
 def cars_get():  # noqa: E501
@@ -10,6 +6,6 @@ def cars_get():  # noqa: E501
     Get a list of all car geolocations # noqa: E501
 
 
-    :rtype: None
+    :rtype: Cars
     """
     return 'do some magic!'
