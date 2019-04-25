5497c028f15d01bd7733b6ec80b4d10e2ed047af
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2019-04-25 15:56:17 +0200

    Included ag-Grid functionality.

--- src/app/modules/info/info.component.ts
+++ src/app/modules/info/info.component.ts
@@ -14,4 +14,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class InfoComponent implements OnInit {
   title: string = 'Car info';
+  rowData: any;
+  
+  columnDefs = [
+    { headerName: 'Make', field: 'make', sortable: true, filter: true },
+    { headerName: 'Model', field: 'model', sortable: true, filter: true },
+    { headerName: 'Price', field: 'price', sortable: true, filter: true }
+  ];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
 }
