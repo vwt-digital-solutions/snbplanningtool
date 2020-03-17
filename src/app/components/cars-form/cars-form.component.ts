5c81164b577ef7cefe068acf78550c7aca3e1f66
Selene Blok <selene.blok@gmail.com>
2020-03-17 14:02:37 +0100

    DAT-2249 Ag-grid now supports adding cars with klantteam

--- src/app/components/cars-form/cars-form.component.ts
+++ src/app/components/cars-form/cars-form.component.ts
@@ -6,6 +6,7 @@ import { Car } from 'src/app/classes/car';
 import { ApiService } from 'src/app/services/api.service';
 import { CarsService } from 'src/app/services/cars.service';
 import {CarProviderService} from '../../services/car-provider.service';
+import { MapGeometryObject } from '../../classes/map-geometry-object';
 
 @Component({
   selector: 'app-cars-form',
@@ -19,6 +20,7 @@ export class CarsFormComponent implements OnInit {
   buttonSave = 'Opslaan';
 
   carsTokens: object;
+  administrations = MapGeometryObject.administrations;
 
   model = new Car(null, '', '', '', '', '', null);
 
