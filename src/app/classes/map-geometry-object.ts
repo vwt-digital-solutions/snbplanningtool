7142dcf4edc91e2e8d33ba927c9517f0f126eaea
Selene Blok <selene.blok@gmail.com>
2020-03-17 12:57:51 +0100

    DAT-2254 Added administrations to map-geometry-object
    The administrations can now be edited here and will be immediately reflected

--- src/app/classes/map-geometry-object.ts
+++ src/app/classes/map-geometry-object.ts
@@ -1,6 +1,29 @@
 import * as L from 'leaflet';
 
 export abstract class MapGeometryObject {
+  public static administrations = [
+    'Administratie niet kunnen bepalen',
+    'Operations Noord Klantteam 01 (NHL)',
+    'Operations Noord Klantteam 02 (AMS)',
+    'Operations Noord Klantteam 03 (UTR)',
+    'Operations Noord Klantteam 04 (FRL)',
+    'Operations Noord Klantteam 05 (GRN)',
+    'Operations Noord Klantteam 06 (OVL)',
+    'Operations Zuid Klantteam 07 (DHG)',
+    'Operations Zuid Klantteam 08 (RTD)',
+    'Operations Zuid Klantteam 09 (ZLD)',
+    'Operations Zuid Klantteam 10 (NBR)',
+    'Operations Zuid Klantteam 11 (ANH)',
+    'Operations Zuid Klantteam 12 (LIM)',
+    'VWT Control',
+    'VWT NOC Planning',
+    'VWT NOC Projectteam',
+    'VWT NOC Servicedesk en Vergunningenbureau',
+    'VWT NOC Werkvoorbereiding/Engineering',
+    'VWT Operations Noord Algemeen',
+    'VWT Operations Zuid Algemeen',
+  ];
+
   constructor(public geometry: any) {}
 
   options = {
